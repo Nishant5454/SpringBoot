a18a12c6de5fa3dcbeb531df005135dea9cc3900
Nishant5454 <bhagatnishant5454@gmail.com>
2025-02-21 14:57:57 +0530

    [Nishant]Added the main branch

---
+++ src/main/java/myfirst/hihello/HihelloApplication.java
@@ -0,0 +1,13 @@
+package myfirst.hihello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HihelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HihelloApplication.class, args);
+	}
+
+}
