94df5e172d74601aeabaf11159e5aa2ca93d9926
Nishant5454 <bhagatnishant5454@gmail.com>
2025-02-21 12:24:51 +0530

    [Nishant]Add .HelloWorld

---
+++ src/test/java/myfirst/hihello/HihelloApplicationTests.java
@@ -0,0 +1,13 @@
+package myfirst.hihello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HihelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
